609cc5169637ba287e1de3de6167a1a770b7b9f6
ohmyguigs <ohmyguigs@gmail.com>
2018-12-13 06:11:06 -0200

    Enhance images loading time

--- react/index.js
+++ react/index.js
@@ -1,13 +1,34 @@
 import React, { Component, Fragment } from 'react'
 import { Helmet } from 'render'
+import { Spinner } from 'vtex.styleguide' 
 import { version } from '../manifest.json'
 import './global.css'
 const SERIOUS_BLACK = '#142032'
-const TOTAL_RENDERED_FERAS = 50
+const PERCENTAGE_OF_HATLESS_FERA = 82
+const TOTAL_RENDERED_FERAS = 100
+const TOTAL_IMAGES_IN_ASSETS = 11
 
 const stringifyIntWithTwoDigits = (int) => {
   return ('0' + int).slice(-2)
 }
+
+const getRandomInt = (from, to) => {
+  const min = Math.ceil(from)
+  const max = Math.floor(to)
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+const IMAGES = new Array(TOTAL_IMAGES_IN_ASSETS).fill(null).map((_null, i) => require(`./assets/fera-hat-${i}.png`))
+const FERAS_IN_HATS = new Array(TOTAL_RENDERED_FERAS).fill(null).map((_null, i) => {
+  const hatIndex = (Math.random() <= PERCENTAGE_OF_HATLESS_FERA / 100) ? 0 : getRandomInt(0, 10)
+  return (
+    <img
+      key={`fera-${i}`}
+      id={`fera-${i}`}
+      src={IMAGES[hatIndex]}
+      className="fera" />
+  )
+})
 
 class App extends Component {
   constructor() {
@@ -20,31 +41,37 @@ class App extends Component {
       initialSeconds: 0,
       editing: 'minutes',
       intervalReference: null,
-      animatedFerasCache: [],
+      timeEnded: false,
+      firstTimeZero: true,
+      showHelperInfo: false,
+      loading: true,
     }
   }
 
   componentDidMount() {
-    document && document.addEventListener('keyup', (event) => {
-      const { key } = event
-      const { intervalReference, editing } = this.state
-      if (key === ' ' || key === 'Enter') {
-        intervalReference ? this.pauseTimer() : this.startTimer()
-      } else if (key === 'ArrowRight') {
-        this.toggleEdit('seconds')
-      } else if (key === 'ArrowLeft') {
-        this.toggleEdit('minutes')
-      } else if (key === 'ArrowUp') {
-        editing === 'minutes' ? this.updateMinutes(true) : this.updateSeconds(true)
-      } else if (key === 'ArrowDown') {
-        editing === 'minutes' ? this.updateMinutes(false) : this.updateSeconds(false)
-      } else if (key === 'r' || key === 'R') {
-        this.resetTimer()
-      } else if (key === 'v' || key === 'V') {
-        this.handleFeraParade()
-      } else {
-        console.log('pressed unmapped key: ', key)
-      }
+    this.setState({ loading: false }, () => {
+      setTimeout(() => this.setState({ showHelperInfo: true }), 420000)
+      document && document.addEventListener('keyup', (event) => {
+        const { key } = event
+        const { intervalReference, editing } = this.state
+        if (key === ' ' || key === 'Enter') {
+          intervalReference ? this.pauseTimer() : this.startTimer()
+        } else if (key === 'ArrowRight') {
+          this.toggleEdit('seconds')
+        } else if (key === 'ArrowLeft') {
+          this.toggleEdit('minutes')
+        } else if (key === 'ArrowUp') {
+          editing === 'minutes' ? this.updateMinutes(true) : this.updateSeconds(true)
+        } else if (key === 'ArrowDown') {
+          editing === 'minutes' ? this.updateMinutes(false) : this.updateSeconds(false)
+        } else if (key === 'r' || key === 'R') {
+          this.resetTimer()
+        } else if (key === 'v' || key === 'V') {
+          this.animateRandomFera()
+        } else {
+          console.log('pressed unmapped key: ', key)
+        }
+      })
     })
   }
 
@@ -63,6 +90,10 @@ class App extends Component {
       this.setState({
         minutes: shouldIncrement ? minutes + 1 : minutes - 1,
       })
+    } else if (minutes <= 0 && shouldIncrement) {
+      this.setState({ minutes: minutes + 1, timeEnded: false })
+    } else if (minutes === 99 && !shouldIncrement) {
+      this.setState({ minutes: minutes - 1 })
     }
   }
 
@@ -72,25 +103,51 @@ class App extends Component {
       this.setState({
         seconds: shouldIncrement ? seconds + 1 : seconds - 1,
       })
+    } else if (seconds === 0 && shouldIncrement) {
+      this.setState({ seconds: seconds + 1 })
+    } else if (seconds === 59 && !shouldIncrement) {
+      this.setState({ seconds: seconds - 1 })
     }
   }
 
   startTimer = () => {
-    const { minutes, seconds } = this.state
-    const intervalReference = setInterval(() => {
-      console.log('callback called!')
-      if (seconds === 0) {
-        this.setState({
-          minutes: minutes - 1,
-          seconds: 59,
-        })
-        return
-      }
-      this.setState({
-        seconds: seconds - 1,
-      })
-    }, 1000)
-    this.setState({ intervalReference })
+    const { intervalReference } = this.state
+    if (!intervalReference) {
+      const intervalReference = setInterval(() => {
+        const { seconds } = this.state
+        if (seconds === 0) {
+          this.setState(previousState => {
+            const { minutes, intervalReference, timeEnded } = previousState
+            return {
+              ...previousState,
+              seconds: 59,
+              minutes: minutes - 1,
+              timeEnded: !!intervalReference && minutes === 0,
+              firstTimeZero: !!intervalReference && minutes === 0 && !timeEnded,
+            }
+          })
+        } else {
+          this.setState(previousState => {
+            const { minute, firstTimeZero } = previousState
+            if (minute === 0 && seconds === 1 && firstTimeZero) {
+              return {
+                ...previousState,
+                seconds: previousState.seconds - 1,
+                minutes: 0,
+                timeEnded: true,
+                firstTimeZero: false,
+              }
+            } else {
+              return {
+                ...previousState,
+                seconds: previousState.seconds - 1,
+              }
+            }
+          })
+        }
+      }, 1000)
+      this.setState({ intervalReference })
+    }
   }
 
   pauseTimer = () => {
@@ -109,48 +166,17 @@ class App extends Component {
     })
   }
 
-  handleFeraParade = () => {
-    const { animatedFerasCache } = this.state
-    // animate random fera and cache animated index for 2 seconds (animation time)
-    const random = this.getRandomInt(0, TOTAL_RENDERED_FERAS - 1)
-    if (!animatedFerasCache.includes(random)) {
-
-      const element = document.getElementById(`fera-${random}`)
-      element.className = element.className + ' animateFera'
-
-      const newCache = animatedFerasCache.push(random)
-      this.setState({ animatedFerasCache: newCache }, () => {
-        setTimeout(() => {
-          element.className = element.className.replace(' animateFera', '')
-          const newCache = animatedFerasCache.filter(value => value === random)
-          this.setState({ animatedFerasCache: newCache })
-        }, 2000)
-      })
-    }
+  animateRandomFera = (forceNoHat = false) => {
+    const random = forceNoHat ? 0 : getRandomInt(0, TOTAL_RENDERED_FERAS - 1)
+    const element = document.getElementById(`fera-${random}`)
+    element.className = element.className + ' animateFera'
+    setTimeout(() => {
+      element.className = element.className.replace(' animateFera', '')
+    }, 2000)
   }
 
-  getRandomInt = (from, to) => {
-    const min = Math.ceil(from)
-    const max = Math.floor(to)
-    return Math.floor(Math.random() * (max - min)) + min
-  }
-
-  renderFerasInHats = () => {
-    return new Array(TOTAL_RENDERED_FERAS).fill(null).map((_null, i) => (
-      <img
-        key={`fera-${i}`}
-        id={`fera-${i}`}
-        src={require(`./assets/fera-hat-${
-          i % 2 === 0
-          ? 0 // if i is even render no-hat-fera
-          : this.getRandomInt(0, 10)
-        }.png`)}
-        className="fera" />
-    ))
-  }
-
-  renderHelpInfo = () => (
-    <div className="absolute bottom-1 left-1 tl h-20">
+  renderHelpInfo = () => this.state.showHelperInfo && (
+    <div className="absolute bottom-1 left-1 tl h-20 dn dib-ns">
       <ul className="white f5 list">
         <li>
           <p><span className="b">arrow keys</span> to set timer</p>
@@ -169,21 +195,37 @@ class App extends Component {
   )
 
   render() {
-    const { minutes, seconds, editing, intervalReference } = this.state
+    const { minutes, seconds, editing, intervalReference, timeEnded, firstTimeZero, loading } = this.state
+    if (loading) {
+      return <div className="flex flex-column overflow-hidden">
+        <div className="center mv10" style={{ transform: 'scale(5)' }}>
+          <Spinner />
+        </div>
+        <p className="w-100 center tc c-muted-1 fw3 f3 fw5-ns f1-ns mh7">
+            Fetching data from outside the orbit...
+          </p>
+      </div>
+    }
+
+    if (minutes <= 0 && !firstTimeZero) {
+      if (timeEnded && Math.random() >= 0.420) {
+        this.animateRandomFera()
+      }
+    }
 
     return (
       <Fragment>
         <Helmet>
           <title>Timer Demofriday</title>
         </Helmet>
-        <div className="flex relative vh-100 w-100" style={{ backgroundColor: SERIOUS_BLACK }}>
+        <div className="flex relative overflow-hidden vh-100 w-100" style={{ backgroundColor: SERIOUS_BLACK }}>
           <div className="absolute w-100 vh-100">
             <div className="flex items-center h-100">
               <span
                 className="tc center fw6 white"
                 style={{ fontSize: '25.5vw' }}>
                   <span className={intervalReference || editing === 'minutes' ? 'white' : 'c-muted-2'}>
-                    {stringifyIntWithTwoDigits(minutes)}
+                    {minutes === 0 && firstTimeZero ? '-00' : stringifyIntWithTwoDigits(minutes)}
                   </span>:<span className={intervalReference || editing === 'seconds' ? 'white' : 'c-muted-2'}>
                     {stringifyIntWithTwoDigits(seconds)}
                   </span>
@@ -191,7 +233,7 @@ class App extends Component {
             </div>
           </div>
           {intervalReference ? null : this.renderHelpInfo()}
-          {this.renderFerasInHats()}
+          {FERAS_IN_HATS}
         </div>
       </Fragment>
     )
